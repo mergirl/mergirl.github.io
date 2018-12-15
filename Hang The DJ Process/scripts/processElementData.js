31d27966b6a1cb97fd893adcd2ef9df22bd75b92
Nilson Ferreira <nilson@Mac-mini-de-Nilson-4.local>
2018-12-15 18:55:53 -0300

    Trabalho de Processos de Software

---
+++ Hang The DJ Process/scripts/processElementData.js
@@ -0,0 +1,77 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_4COFsQANEem7o5Qifqdb1Q,_8XF0MQANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Validação Final_9E58DB8B_6952c3d6_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1QActivity"]="./../../hang_the_dj/deliveryprocesses/resources/Hang The DJ Business_944578A2_944578a2_Activity.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_SLRHoQANEem7o5Qifqdb1Q,_mWMv4QANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Validação Primária_CB340B4D_71023585_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_SLRHoQANEem7o5Qifqdb1Q,_kq8vAQANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Orquestração_D36AF1_a6a19529_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_HyJpoQALEem7o5Qifqdb1Q,_WqVwcQAMEem7o5Qifqdb1QActivity"]="./../../hang_the_dj/deliveryprocesses/resources/Identificar_3286F545_37484f03_Activity.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_HyJpoQALEem7o5Qifqdb1QActivity"]="./../../hang_the_dj/deliveryprocesses/resources/Planejar_17352874_767e96a_Activity.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_SLRHoQANEem7o5Qifqdb1Q,_fcTf4QANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Ordenação_2F7E3389_d54c5dc1_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_SLRHoQANEem7o5Qifqdb1QActivity"]="./../../hang_the_dj/deliveryprocesses/resources/Criar_763746EE_666a07e4_Activity.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_4COFsQANEem7o5Qifqdb1QActivity"]="./../../hang_the_dj/deliveryprocesses/resources/Validar_7AB24E01_6ae50ef7_Activity.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_HyJpoQALEem7o5Qifqdb1Q,_ltBsgQAMEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Selecionar_244366DF_2904c09d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_HyJpoQALEem7o5Qifqdb1Q,_JS6m4QANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Ajustar_29FFFF3A_2ec158f8_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_SLRHoQANEem7o5Qifqdb1Q,_imY2QQANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Coreografia_451CEC9A_eaeb16d2_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["__THAwQAKEem7o5Qifqdb1Q,_4COFsQANEem7o5Qifqdb1Q,_6MasYQANEem7o5Qifqdb1QActivityDetail"]="./../../hang_the_dj/deliveryprocesses/resources/Amostragem_7F3AA4BE_4a348d09_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_HiHI4gATEem7o5Qifqdb1Q"] = ["Time de Produção","hang_the_dj/roles/time_de_produo_ABAD5A78.html"];
+contentPage.processPage.elementUrls["_ltBsgQAMEem7o5Qifqdb1Q"] = ["Selecionar","hang_the_dj/deliveryprocesses/Selecionar_244366DF.html"];
+contentPage.processPage.elementUrls["_-sP7cAASEem7o5Qifqdb1Q"] = ["Selecionar","hang_the_dj/tasks/selecionar_34673632.html"];
+contentPage.processPage.elementUrls["_FgyxYAATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_-sP7cQASEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_SLRHoQANEem7o5Qifqdb1Q"] = ["Criar","hang_the_dj/deliveryprocesses/Criar_763746EE.html"];
+contentPage.processPage.elementUrls["_Jkq2ggATEem7o5Qifqdb1Q"] = ["Amostras","hang_the_dj/workproducts/amostras_766F72F1.html"];
+contentPage.processPage.elementUrls["_Fg44AAATEem7o5Qifqdb1Q"] = ["Demo 02","hang_the_dj/workproducts/demo_02_7F7DF04D.html"];
+contentPage.processPage.elementUrls["_HGUkwAATEem7o5Qifqdb1Q"] = ["Lista Chave","hang_the_dj/workproducts/lista_chave_F5EEA4D.html"];
+contentPage.processPage.elementUrls["_HhuuYAATEem7o5Qifqdb1Q"] = ["Coreografia","hang_the_dj/tasks/coreografia_E3CB48CD.html"];
+contentPage.processPage.elementUrls["_WqVwcQAMEem7o5Qifqdb1Q"] = ["Identificar","hang_the_dj/deliveryprocesses/Identificar_3286F545.html"];
+contentPage.processPage.elementUrls["_HF2DoAATEem7o5Qifqdb1Q"] = ["Ordenação","hang_the_dj/tasks/ordenao_4A4D98E8.html"];
+contentPage.processPage.elementUrls["_imY2QQANEem7o5Qifqdb1Q"] = ["Coreografia","hang_the_dj/deliveryprocesses/Coreografia_451CEC9A.html"];
+contentPage.processPage.elementUrls["_J3YXIAATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_JS6m4QANEem7o5Qifqdb1Q"] = ["Ajustar","hang_the_dj/deliveryprocesses/Ajustar_29FFFF3A.html"];
+contentPage.processPage.elementUrls["_J3edwAATEem7o5Qifqdb1Q"] = ["Amostras","hang_the_dj/workproducts/amostras_766F72F1.html"];
+contentPage.processPage.elementUrls["_kq8vAQANEem7o5Qifqdb1Q"] = ["Orquestração","hang_the_dj/deliveryprocesses/Orquestração_D36AF1.html"];
+contentPage.processPage.elementUrls["_mWMv4QANEem7o5Qifqdb1Q"] = ["Validação Primária","hang_the_dj/deliveryprocesses/Validação Primária_CB340B4D.html"];
+contentPage.processPage.elementUrls["_-sP7cwASEem7o5Qifqdb1Q"] = ["Lista Base","hang_the_dj/workproducts/lista_base_3F5FCA.html"];
+contentPage.processPage.elementUrls["_9oVnZAASEem7o5Qifqdb1Q"] = ["Lista Base","hang_the_dj/workproducts/lista_base_3F5FCA.html"];
+contentPage.processPage.elementUrls["_HiHI4AATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_HGIXggATEem7o5Qifqdb1Q"] = ["Time de Produção","hang_the_dj/roles/time_de_produo_ABAD5A78.html"];
+contentPage.processPage.elementUrls["_LPQ-0QANEem7o5Qifqdb1Q"] = ["Ajustar","hang_the_dj/deliveryprocesses/Ajustar_272373E.html"];
+contentPage.processPage.elementUrls["_9oVnYAASEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_HiNPgAATEem7o5Qifqdb1Q"] = ["Ordenado","hang_the_dj/workproducts/ordenado_D98638D4.html"];
+contentPage.processPage.elementUrls["_JkLuUAATEem7o5Qifqdb1Q"] = ["Amostragem","hang_the_dj/tasks/amostragem_C50CE21B.html"];
+contentPage.processPage.elementUrls["_-JksMAASEem7o5Qifqdb1Q"] = ["Identificar","hang_the_dj/tasks/identificar_4996626F.html"];
+contentPage.processPage.elementUrls["_GjpVgAATEem7o5Qifqdb1Q"] = ["Orquestração","hang_the_dj/tasks/orquestrao_B3CC26C0.html"];
+contentPage.processPage.elementUrls["_9oVnYgASEem7o5Qifqdb1Q"] = ["Time de Produção","hang_the_dj/roles/time_de_produo_ABAD5A78.html"];
+contentPage.processPage.elementUrls["_HGIXgAATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_GkBwAAATEem7o5Qifqdb1Q"] = ["Demo 01","hang_the_dj/workproducts/demo_01_8BC6C31F.html"];
+contentPage.processPage.elementUrls["_J3GDQAATEem7o5Qifqdb1Q"] = ["Validação Final","hang_the_dj/tasks/validao_final_D6F8E371.html"];
+contentPage.processPage.elementUrls["_Gj7pYgATEem7o5Qifqdb1Q"] = ["Time de Produção","hang_the_dj/roles/time_de_produo_ABAD5A78.html"];
+contentPage.processPage.elementUrls["_HiNPggATEem7o5Qifqdb1Q"] = ["Demo 01","hang_the_dj/workproducts/demo_01_8BC6C31F.html"];
+contentPage.processPage.elementUrls["_JkeCMAATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["__THAwQAKEem7o5Qifqdb1Q"] = ["Hang The DJ Business","hang_the_dj/deliveryprocesses/Hang The DJ Business_944578A2.html"];
+contentPage.processPage.elementUrls["_9oVnZgASEem7o5Qifqdb1Q"] = ["Lista Chave","hang_the_dj/workproducts/lista_chave_F5EEA4D.html"];
+contentPage.processPage.elementUrls["_Gj7pYAATEem7o5Qifqdb1Q"] = ["Produtor Chefe","hang_the_dj/roles/produtor_chefe_AB38A56F.html"];
+contentPage.processPage.elementUrls["_Jkq2gAATEem7o5Qifqdb1Q"] = ["Remix/Set","hang_the_dj/workproducts/remixset_ED750DCD.html"];
+contentPage.processPage.elementUrls["_Fg--oQATEem7o5Qifqdb1Q"] = ["Remix/Set","hang_the_dj/workproducts/remixset_ED750DCD.html"];
+contentPage.processPage.elementUrls["_4COFsQANEem7o5Qifqdb1Q"] = ["Validar","hang_the_dj/deliveryprocesses/Validar_7AB24E01.html"];
+contentPage.processPage.elementUrls["_GkBwAgATEem7o5Qifqdb1Q"] = ["Demo 02","hang_the_dj/workproducts/demo_02_7F7DF04D.html"];
+contentPage.processPage.elementUrls["_HyJpoQALEem7o5Qifqdb1Q"] = ["Planejar","hang_the_dj/deliveryprocesses/Planejar_17352874.html"];
+contentPage.processPage.elementUrls["_9oDTgAASEem7o5Qifqdb1Q"] = ["Ajustar","hang_the_dj/tasks/ajustar_89CD63D8.html"];
+contentPage.processPage.elementUrls["_HGUkwgATEem7o5Qifqdb1Q"] = ["Ordenado","hang_the_dj/workproducts/ordenado_D98638D4.html"];
+contentPage.processPage.elementUrls["_FgyxYgATEem7o5Qifqdb1Q"] = ["Time de Produção","hang_the_dj/roles/time_de_produo_ABAD5A78.html"];
+contentPage.processPage.elementUrls["_J3YXIgATEem7o5Qifqdb1Q"] = ["Pocs","hang_the_dj/roles/pocs_80B024AB.html"];
+contentPage.processPage.elementUrls["_fcTf4QANEem7o5Qifqdb1Q"] = ["Ordenação","hang_the_dj/deliveryprocesses/Ordenação_2F7E3389.html"];
+contentPage.processPage.elementUrls["_FgmkIAATEem7o5Qifqdb1Q"] = ["Validação Primária","hang_the_dj/tasks/validao_primria_C1777E5F.html"];
+contentPage.processPage.elementUrls["_6MasYQANEem7o5Qifqdb1Q"] = ["Amostragem","hang_the_dj/deliveryprocesses/Amostragem_7F3AA4BE.html"];
+contentPage.processPage.elementUrls["_8XF0MQANEem7o5Qifqdb1Q"] = ["Validação Final","hang_the_dj/deliveryprocesses/Validação Final_9E58DB8B.html"];
